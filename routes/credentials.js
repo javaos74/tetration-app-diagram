270180773552e3d8b71aac5b7b2b124adb340179
HANG RAE CHO <JO@HANGs-MacBook-Pro.local>
2017-10-27 17:09:24 +0900

    icon,table title, dateformat

--- routes/credentials.js
+++ routes/credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_ENDPOINT : "https://TETRATION_CLUSTER_ENDPOINT.HERE",
-  API_KEY      : "APIKEY HERE ---",
-  API_SECRET   : "API_SECRET HERE ---"
+  API_ENDPOINT : "https://overlord.cam.ciscolabs.com",
+  API_KEY      : "450ed45095c740f683c9688898293152",
+  API_SECRET   : "162c67e140543cb1c9ea392c51f295d560db31ac"
 }
