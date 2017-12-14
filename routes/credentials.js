2611efeac46113f199abbc588d512efd6d1114cd
Hyung-Soo Kim <hyungsok@cisco.com>
2017-12-14 14:43:15 +0900

    trivial

--- routes/credentials.js
+++ routes/credentials.js
@@ -1,5 +1,30 @@
 module.exports = {
-  API_ENDPOINT : "https://overlord.cam.ciscolabs.com",
-  API_KEY      : "450ed45095c740f683c9688898293152",
-  API_SECRET   : "162c67e140543cb1c9ea392c51f295d560db31ac"
+  API_ENDPOINT : "https://ta.shinsegae.com",
+  API_KEY: "0dadba63e27e4d78bc52a656f0a133fd",
+  API_SECRET: "f690a8ea158de27657ace978d272d683152fbb54"
 }
+
+// medusa
+//module.exports = {
+//  API_ENDPOINT : "https://medusa-cpoc.cisco.com",
+//  API_KEY: "095b398501f941309d24410f05b16557",
+//  API_SECRET: "9db44e43e1a7ba474b8263da5d49fd1649b93add"
+//}
+
+//module.exports = {
+//  API_ENDPOINT : "https://overlord.cam.ciscolabs.com",
+//  API_KEY      : "450ed45095c740f683c9688898293152",
+//  API_SECRET   : "162c67e140543cb1c9ea392c51f295d560db31ac"
+//}
+
+// MyCluster 10.72.85.4
+// "api_key": "3e331b24bdd646d8917332307917be2b",
+// "api_secret": "59f6919e70db1b868372cdb44360d5f53176d21e"
+//
+
+
+// Shinsegae cluster 10.253.100.4
+// API Key: 0dadba63e27e4d78bc52a656f0a133fd
+// API Secret: f690a8ea158de27657ace978d272d683152fbb54 
+
+
