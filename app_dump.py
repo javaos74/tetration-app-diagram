a5c52f87d25765c2889ed3baea3354cab6b7a084
Hyung-Soo Kim <hyungsok@cisco.com>
2017-12-14 14:44:29 +0900

    update for test

---
+++ app_dump.py
@@ -0,0 +1,41 @@
+from tetpyclient import RestClient
+import pprint
+import json
+
+API_ENDPOINT="https://ta.shinsegae.com"
+
+# ``verify`` is an optional param to disable SSL server authentication.
+# By default, Tetration appliance dashboard IP uses self signed cert after
+# deployment. Hence, ``verify=False`` might be used to disable server
+# authentication in SSL for API clients. If users upload their own
+# certificate to Tetration appliance (from ``Settings > Company`` Tab)
+# which is signed by their enterprise CA, then server side authentication
+# should be enabled.
+# credentials.json looks like:
+# {
+#   "api_key": "<hex string>",
+#   "api_secret": "<hex string>"
+# }
+
+restclient = RestClient(API_ENDPOINT,
+                credentials_file='api_cred.json',
+                verify=False)
+
+apps = []
+resp = restclient.get('/applications')
+if resp.status_code == 200 :
+    respbody = json.loads(resp.text)
+    with open('public/data/all.json', 'w+') as appf:
+        appf.write( resp.text)
+        respbody = json.loads( resp.text)
+        for app in respbody:
+            appnames = {}
+            appnames['id'] = str(app['id'])
+            appnames['name'] = str(app['name'])
+            apps.append( appnames)
+
+for app in apps:
+    resp = restclient.get('/applications/%s/details' %(app['id']))
+    if resp.status_code == 200:
+        with open('public/data/%s.json' %(app['id']), "w+") as outf:
+            outf.write( resp.text)
